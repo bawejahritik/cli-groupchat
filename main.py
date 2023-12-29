663616e7abe99cd2fdd248bce6e2af305672cbf9
bawejahritik <bawejahritik@gmail.com>
2023-12-29 17:44:03 -0500

    add username functionality

--- main.py
+++ main.py
@@ -12,38 +12,57 @@ def receive():
         while True:
             message = p.get_message()
             if message:
-                # do something with the message
                 message = message["data"]
-                print(message)
+                if type(message) == str:
+                # do something with the message
+                    i = 0
+                    curr = message[i]
+                    lengthOfUserName = 0
+                    while curr.isnumeric():
+                        lengthOfUserName = lengthOfUserName*10 + int(curr)
+                        i += 1
+                        curr=message[i]
+                    print(f'{message[i:i+lengthOfUserName]} -> {message[lengthOfUserName + i : ]}')
             time.sleep(0.001)
 
 def write(username):
     r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-    while True:                                                 #message layout
-        message = input('')
-        # r.publish('my-channel',str(len(username)) + username + message)
-        r.publish('my-channel',{username: username, message: message})
+    try:
+        while True:                                                 #message layout
+            message = input('')
+            r.publish('my-channel',str(len(username)) + username + message)
+            # r.publish('my-channel',{username: username, message: message})
+    except:
+        pass
 
 @click.command()
 @click.option('-s', '--server', 'server', is_flag=True, help="To start the server")
 @click.option('-c', '--client', 'client', is_flag=True, help="To start the client")
 def main(server, client):
     r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-    
-    if server:
-        username = input("Enter your username: ")
-        while True:
-            msg = input()
-            r.publish("my-channel", str(len(username)) + username + msg)
+    username = input("Enter your username: ")
+    receive_thread = threading.Thread(target=receive)               #receiving multiple messages
+    receive_thread.start()
+    write_thread = threading.Thread(target=write, args=(username, ))                   #sending messages 
+    write_thread.start()
+    # if server:
+    #     username = input("Enter your username: ")
+    #     receive_thread = threading.Thread(target=receive)               #receiving multiple messages
+    #     receive_thread.start()
+    #     write_thread = threading.Thread(target=write, args=(username, ))                   #sending messages 
+    #     write_thread.start()
+    #     # while True:
+    #     #     msg = input()
+    #     #     r.publish("my-channel", str(len(username)) + username + msg)
 
     
-    if client:
-          # be nice to the system :)
-        username = input("Enter your username: ")
-        receive_thread = threading.Thread(target=receive)               #receiving multiple messages
-        receive_thread.start()
-        write_thread = threading.Thread(target=write, args=(username, ))                   #sending messages 
-        write_thread.start()
+    # if client:
+    #       # be nice to the system :)
+    #     username = input("Enter your username: ")
+    #     receive_thread = threading.Thread(target=receive)               #receiving multiple messages
+    #     receive_thread.start()
+    #     write_thread = threading.Thread(target=write, args=(username, ))                   #sending messages 
+        # write_thread.start()
 
 if __name__ == '__main__':
     main()
